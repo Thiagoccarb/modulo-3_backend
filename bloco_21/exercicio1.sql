e73df7ef8de544910247ffdfee468f352635d1f9
Thiago <thiagoc.carboneri@gmail.com>
2021-11-04 19:09:16 -0300

    exercicios do dia

--- bloco_21/exercicio1.sql
+++ bloco_21/exercicio1.sql
@@ -4,7 +4,6 @@
 --Sexo;
 --Idade;
 --Localização;
-
 --Cada animal também possui vários cuidadores, e cada cuidador pode ser responsável por mais de um animal. Além disso, cada cuidador possui um gerente, sendo que cada gerente pode ser responsável por mais de um cuidador.
 --Identificação das Entidades :
 --Entidade 1: Animal ;
@@ -21,36 +20,31 @@
 --Um cuidador pode cuidar de um ou mais animais;
 --Um gerente pode gerenciar uma ou mais cuidadores;
 --Um cuidador possui um gerente;
-
 DROP SCHEMA IF EXISTS zoologico;
-    CREATE SCHEMA zoologico;
-    USE zoologico;
-
-    CREATE TABLE animal(
-        animal_id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(50) NOT NULL,
-        especie VARCHAR(50) NOT NULL,
-        sexo VARCHAR(50) NOT NULL,
-        idade INT NOT NULL,
-        localizacao VARCHAR(50) NOT NULL
-    );
-
-     CREATE TABLE gerente(
-        gerente_id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(50) NOT NULL
-    );
-
-    CREATE TABLE cuidador(
-        cuidador_id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(50) NOT NULL,
-        gerente_id INT NOT NULL,
-        FOREIGN KEY (gerente_id) REFERENCES gerente (gerente_id)
-    );
-
-    CREATE TABLE animal_cuidador(
-        animal_id INT,
-        cuidador_id INT,
-        CONSTRAINT PRIMARY KEY(animal_id, cuidador_id),
-        FOREIGN KEY (animal_id) REFERENCES animal (animal_id),
-        FOREIGN KEY (cuidador_id) REFERENCES cuidador (cuidador_id)
-    );
+CREATE SCHEMA zoologico;
+USE zoologico;
+CREATE TABLE animal(
+  animal_id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  especie VARCHAR(50) NOT NULL,
+  sexo VARCHAR(50) NOT NULL,
+  idade INT NOT NULL,
+  localizacao VARCHAR(50) NOT NULL
+);
+CREATE TABLE gerente(
+  gerente_id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL
+);
+CREATE TABLE cuidador(
+  cuidador_id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  gerente_id INT NOT NULL,
+  FOREIGN KEY (gerente_id) REFERENCES gerente (gerente_id)
+);
+CREATE TABLE animal_cuidador(
+  animal_id INT,
+  cuidador_id INT,
+  CONSTRAINT PRIMARY KEY(animal_id, cuidador_id),
+  FOREIGN KEY (animal_id) REFERENCES animal (animal_id),
+  FOREIGN KEY (cuidador_id) REFERENCES cuidador (cuidador_id)
+);
